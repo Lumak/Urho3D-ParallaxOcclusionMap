6bb52e0ab00a15d807a5d164a6db34c4ce677bd8
Lumak <Lumak@users.noreply.github.com>
2020-06-08 16:45:48 -0700

    * added elix22's transpose fn for glsl, added cube parallax sample in scene0

--- bin/Data/Parallax/Shaders/GLSL/LitSolidParallax.glsl
+++ bin/Data/Parallax/Shaders/GLSL/LitSolidParallax.glsl
@@ -6,6 +6,22 @@
 #include "Fog.glsl"
 #include "Parallax.glsl"
 
+#ifdef MOBILE_GRAPHICS
+mat3 transpose(in mat3 inMatrix) {
+     vec3 i0 = inMatrix[0];
+     vec3 i1 = inMatrix[1];
+     vec3 i2 = inMatrix[2];
+
+     mat3 outMatrix = mat3(
+                 vec3(i0.x, i1.x, i2.x),
+                 vec3(i0.y, i1.y, i2.y),
+                 vec3(i0.z, i1.z, i2.z)
+                 );
+
+    return outMatrix;
+}
+#endif
+
 #if defined(NORMALMAP) || defined(PARALLAXMAP)
     varying vec4 vTexCoord;
     varying vec4 vTangent;
